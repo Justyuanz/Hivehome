ad9fa4c4c25281e887d2003fc3c89907d9164a2a
Jingyuan Zhang <justyuan.z@gmail.com>
2025-04-30 14:39:36 +0300

    missing space fixed

--- libft/ft_lstadd_front_bonus.c
+++ libft/ft_lstadd_front_bonus.c
@@ -6,7 +6,7 @@
 /*   By: jinzhang <jinzhang@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/28 19:07:52 by jinzhang          #+#    #+#             */
-/*   Updated: 2025/04/29 16:07:37 by jinzhang         ###   ########.fr       */
+/*   Updated: 2025/04/30 14:39:08 by jinzhang         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,12 @@
 C always passes by value (copy).
 If you return a new value, and reassign, you can still "update" the original.
 If you want to directly modify the original inside the function without return,
-you must pass pointer(the address of the value)
+you must pass	pointer(the address of the value)
 */
 void	ft_lstadd_front(t_list **lst, t_list *new)
 {
-	if(!lst || !new)
-		return;
+	if (!lst || !new)
+		return ;
 	new->next = *lst;
 	*lst = new;
 }
